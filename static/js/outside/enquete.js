acf853dbab22c70cc483aca96ae0637e6ad71d6a
Daniele Guido <gui.daniele@gmail.com>
2012-11-30 16:06:44 +0100

    Events coding

--- static/js/outside/enquete.js
+++ static/js/outside/enquete.js
@@ -10,7 +10,7 @@ oo.enq.d3layer = function() {
 
     var div = d3.select(document.body)
         .append("div")
-        .attr('class', 'd3-vec'),
+        .attr('class', 'd3_layer'),
         svg = div.append('svg'),
         g = svg.append("g");
 
@@ -29,7 +29,16 @@ oo.enq.d3layer = function() {
           .style("margin-top", "0px") && (first = false);
 
       path = d3.geo.path().projection(f.project);
-      feature.attr("d", path);
+
+      feature.attr("d", path)
+      	.attr('lon', function(d, i) {
+      		var coordinates = collection.features[i].geometry.coordinates[0];
+      		return coordinates;
+      	})
+      	.attr('lat', function(d, i) {
+      		var coordinates = collection.features[i].geometry.coordinates[1];
+      		return coordinates;
+      	});
     };
 
     f.data = function(x) {
@@ -37,7 +46,21 @@ oo.enq.d3layer = function() {
         bounds = d3.geo.bounds(collection);
         feature = g.selectAll("path")
             .data(collection.features)
-            .enter().append("path");
+            .enter().append("path")
+            .on("click", function(d,i) {
+            	// console.log('f.map.extent()', f.map.extent())
+            	var position = {
+            		'lat' : d3.select(this).attr('lat'),
+            		'lon' : d3.select(this).attr('lon')
+            	};
+            	f.map.center(position, true);
+
+            	// console.log('f.map.extent() - start', f.map.extent())
+				setTimeout( function() {
+	        		// console.log('f.map.extent() - end  ', f.map.extent())
+	        		oo.filt.trigger( oo.filt.events.add, {'extent':f.map.extent()} );
+	        	}, 1000 );
+            });
         return f;
     };
 
@@ -56,72 +79,111 @@ oo.enq.init = function(){
 	
 	d3.json( oo.api.urlfactory( oo.urls.get_enquete_data, 88 ), function(collection) {
 
-			// Geojson building
 
-			var result = [];
-			for (var i in collection.documents) {
-				result.push(JSON.stringify(collection.documents[i].coordinates));
-			}
-			var jsonObj = '{"type": "FeatureCollection", "features": [' + result.toString() + ']}';
-			collectionGeo = JSON.parse(jsonObj);
+		// Geojson building
 
-			// Map
-
-			mapbox.auto('map', 'examples.map-vyofok3q', function(map) {
-			    layer = oo.enq.d3layer().data(collectionGeo);
-			    map.addLayer(layer);
-			    map.extent(layer.extent());
-			});
-
-			// Timeline
-
-			var format = d3.time.format("%Y-%m-%d");
-
-			var data = d3.range(collection.documents.length).map(function(i) {
-				return {
-					x: format.parse(collection.documents[i].times[0].time),
-					y: Math.floor(Math.random()*5)
-				};
-			});
-
-			var width = $('#map').width(),
-				height = $('#map').height(),
-				margin = {top: 20, right: 10};
+		var result = [];
+		for (var i in collection.documents) {
+			result.push(JSON.stringify(collection.documents[i].coordinates));
+		}
+		var jsonObj = '{"type": "FeatureCollection", "features": [' + result.toString() + ']}';
+		collectionGeo = JSON.parse(jsonObj);
 
 
-			var minX = d3.min(data, function (d) { return d.x });
-			var maxX = d3.max(data, function (d) { return d.x });
-			var minY = d3.min(data, function (d) { return d.y });
-			var maxY = d3.max(data, function (d) { return d.y });
+		// Map
 
-			var scaleX = d3.scale.linear().domain([minX, maxX]).range([ 0, width - margin.right*2 ]);
-			var scaleY = d3.scale.linear().domain([minY, maxY]).range([0, 20]);
+	    var map = mapbox.map('map');
+	    map.addLayer(mapbox.layer().id('examples.map-vyofok3q'));
 
-			var timeline = d3.select('#timeline').append('svg').append("g");
+	    layer = oo.enq.d3layer().data(collectionGeo);
+		map.addLayer(layer);
+		map.extent(layer.extent());
 
-			timeline.attr("transform", "translate(" + margin.right + "," + margin.top + ")");
-
-			timeline.selectAll(".dot")
-				.data(data)
-				.enter().append("circle")
-				.attr('class', 'dot')
-				.attr("cx", function(d) { return scaleX(d.x); })
-				.attr("cy", function(d) { return scaleY(d.y); })
-				.attr("data-time", function(d) { return d.x; })
-				.attr("r", 4);
+		// map.ui.zoomer.add();
+  //   	map.ui.zoombox.add();
 
 
-			// Behaviors
+    	// Behaviors
+			
+		map.addCallback('panned', function(map, panOffset) {
+			// This is a stream of several extent
+			// console.log('f.map.extent()', map.extent())
+			oo.filt.trigger( oo.filt.events.add, {'extent':map.extent()} );
+		});
 
-			$('#map').on('click', 'path', function() {
-				oo.filt.trigger( oo.filt.events.add, {'place':['Paris']} );
-			});
+		map.addCallback('zoomed', function(map, zoomOffset) {
+			// console.log('f.map.extent() - start', map.extent())
+			setTimeout( function() {
+        		// console.log('f.map.extent() - end  ', f.map.extent())
+        		oo.filt.trigger( oo.filt.events.add, {'extent':map.extent()} );
+        	}, 1000 );
+		});
 
-			$('#timeline').on('click', 'circle', function() {
-				var time = $(this).attr('data-time');
-				oo.log(time)
-				oo.filt.trigger( oo.filt.events.add, {'time':[time]} );
-			});
+
+
+		
+
+		// Timeline
+
+		var format = d3.time.format("%Y-%m-%d");
+
+		var data = d3.range(collection.documents.length).map(function(i) {
+			return {
+				x: format.parse(collection.documents[i].times[0].time),
+				y: Math.floor(Math.random()*5)
+			};
+		});
+
+		var width = $('#map').width(),
+			height = $('#map').height(),
+			margin = {top: 20, right: 10};
+
+		var minX = d3.min(data, function (d) { return d.x }),
+			maxX = d3.max(data, function (d) { return d.x }),
+			minY = d3.min(data, function (d) { return d.y }),
+			maxY = d3.max(data, function (d) { return d.y }),
+			scaleX = d3.scale.linear().domain([minX, maxX]).range([ 0, width - margin.right*2 ]),
+			scaleY = d3.scale.linear().domain([minY, maxY]).range([0, 20]);
+
+		var timeline = d3.select('#timeline').append('svg').append("g")
+			.attr("transform", "translate(" + margin.right + "," + margin.top + ")");
+
+		timeline.selectAll(".dot")
+			.data(data)
+			.enter().append("circle")
+			.attr('class', 'dot')
+			.attr("cx", function(d) { return scaleX(d.x); })
+			.attr("cy", function(d) { return scaleY(d.y); })
+			.attr("data-time", function(d) { return d.x; })
+			.attr("r", 4);
+
+		var brush = d3.svg.brush()
+		    .x(scaleX)
+		    .on("brush", brush);
+
+		timeline.append("g")
+	      .attr("class", "x brush")
+	      .call(brush)
+	    .selectAll("rect")
+	      // .attr("x", 0)
+	      .attr("y", -margin.top)
+	      // .attr('width', $('#timeline').width())
+	      .attr("height", $('#timeline').height());
+
+
+		// Behaviors
+
+		$('#timeline').on('click', 'circle', function() {
+			var time = $(this).attr('data-time');
+			// oo.log(time)
+			oo.filt.trigger( oo.filt.events.add, {'time':[time]} );
+		});
+
+		// function brush() {
+		//   x.domain(brush.empty() ? x2.domain() : brush.extent());
+		//   focus.select("path").attr("d", area);
+		//   focus.select(".x.axis").call(xAxis);
+		// }
 
 	});
 }
