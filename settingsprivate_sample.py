e297cfecabf9d61e8d6c3d0dc2c920e2374bdff3
Paul Girard <pgi@townshend.dime-shs.sciences-po.fr>
2013-01-23 17:15:32 +0100

    clean apache conf drectory + created settingsprivate_sample.py

---
+++ settingsprivate_sample.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+REANALYSEURL = 'your app url'
+REANALYSEPROJECTPATH = 'full path to your app'
+REANALYSEESE_FILES = 'full path to ESE files'
+REANALYSESAMPLE_STUDIES_FILES = 'full path to sample studies files'
+ALLOWED_INCLUDE_ROOTS = (REANALYSEESE_FILES,)
+READB_NAME = 'db name'
+READB_USER = 'db user'
+READB_PASS = 'db pass'
+
+STAFF_EMAIL = 'admin email'
+EMAIL_HOST = 'smtp server'
+EMAIL_PORT = 25
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'unique key'
