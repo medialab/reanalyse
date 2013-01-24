9073585478492adb6101c98beef3a0a5ccae98a6
Daniele Guido <gui.daniele@gmail.com>
2013-01-24 23:56:33 +0100

    Active Docs Counter

--- static/js/outside/enq_documents.js
+++ static/js/outside/enq_documents.js
@@ -10,35 +10,44 @@ oo.enq.docs.update = function( event, filters ){
 
 	var docs = d3.select('#documents');
 
-	var items = docs.selectAll('li').each(function() {
+	var items = docs.selectAll('li').each(function(d, i) {
 		item = d3.select(this);
 		item.attr('data-status-old', item.attr('data-status'));
 	}); // Copy new status to old status
 
-	for( var i in oo.filt.data ){
-		if ( oo.filt.data[ i ].filtered == true ) {
-			docs.select('li[data-id="' + oo.filt.data[i].id + '"]').attr('data-status', 'active');
+	var counter = 0;
+
+	for ( var i in oo.filt.data ) {
+		var item = docs.select('li[data-id="' + oo.filt.data[i].id + '"]');
+		if ( oo.filt.data[ i ].filtered ) {
+			item.attr('data-status', 'active');
+			counter++;
 		} else {
-			docs.select('li[data-id="' + oo.filt.data[i].id + '"]').attr('data-status', 'inactive');
+			item.attr('data-status', 'inactive');	
 		}
 	} // Set active
 
+
 	items.each(function() {
 
-		var item = d3.select(this);
+		var item = d3.select(this),
+			oldStatus = item.attr('data-status-old'),
+			newStatus = item.attr('data-status');
 
-		if ( (item.attr('data-status-old') == 'active') && (item.attr('data-status') == 'inactive') ) {
+		if ( oldStatus == 'active' && newStatus == 'inactive' ) {
 			item.transition()
 				.duration(1000)
 				.style('opacity', '0')
 				.style('height', '0px');
-		} else if ( (item.attr('data-status-old') == 'inactive') && (item.attr('data-status') == 'active') ) {
+		} else if ( oldStatus == 'inactive' && newStatus == 'active' ) {
 			item.transition()
 				.duration(1000)
 				.style('opacity', '1')
 				.style('height', '22px');
 		} 
 	})
+
+	d3.select('#counter').html(counter);
 	
 };
 
@@ -46,9 +55,10 @@ oo.enq.docs.init = function ( objects ){
 
 	oo.filt.on( oo.filt.events.change, oo.enq.docs.update );
 
-	var docs = d3.select('#documents');
+	var container = d3.select('#right-sidebar'),
+		docs = container.select('#documents');
 
-	docs.selectAll("li")
+	var li = docs.selectAll("li")
 		.data(objects)
 		.enter().append("li")
 		.attr('class', 'active')
@@ -56,5 +66,7 @@ oo.enq.docs.init = function ( objects ){
 		.attr('data-status', 'active')
 		.html(function(d) { return d.title.split('_').join(' ').split('/').join(' '); });
 
+	var counter = container.append('div').attr('id', 'counter').html(li[0].length);
+	
 };
 
