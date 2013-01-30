b438a73a562c90c4ec0fd186e793011c617848d9
Daniele Guido <gui.daniele@gmail.com>
2013-01-30 18:24:07 +0100

    settings changed and utils.py as well.

--- settings.py
+++ settings.py
@@ -43,7 +43,7 @@ SOLR_PORT = 8986
 
 ######## HAYSTACK
 # Required and specific to where you place the file.
-HAYSTACK_SITECONF = 'reanalyse.search_sites'
+HAYSTACK_SITECONF = ROOT_DIRECTORY_NAME + '.search_sites'
 
 # Optional Haystack settings.
 # See `docs/settings.rst` for a complete list.
@@ -190,7 +190,7 @@ MIDDLEWARE_CLASSES = (
 	'django.middleware.locale.LocaleMiddleware',
 )
 
-ROOT_URLCONF = 'reanalyse.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
 	os.path.join(REANALYSEPROJECTPATH, 'templates'),
